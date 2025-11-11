9db2939b4727c8d607ec1d6e0c6a2e912760339e
YasoKuhl1710 <luca.weissinger1710@gmail.com>
2025-11-11 13:34:50 +0100

    hinzufügen von 2.17.1

---
+++ Aufgabe 2/2.17.1/2.17.1.ino
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid ="";
+const char *password ="";
+
+const char *broker ="";
+const char *topic ="";
+const char *username ="";
+const char *mqtt_password ="";
+const int port =;
+const int Taster1 = 36;
+const int time1 = 0;
+const int time2 = 0;
+const int buttonState = 0;
+const int lastButtonState = 0;
+const unsigned long pressTime = 0;
+const unsigned long releaseTime = 0;
+const unsigned long duration = 0;
+const long Timeout = 500;
+const long Timer = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  client.setServer(broker, port);
+  client.setCallback(callback);
+  while (!client.connected())
+  {
+    String client_id = "esp32-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to broker \n",client_id.c_str());
+    if (client.connect(client_id.c_str(),username,password))
+    {
+      Serial.println("broker connected");
+    }
+    else 
+    {
+      Serial.print("failed with state");
+      Serial.print(client.state());
+      delay(2000);
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(36);
+  time1 = 0;
+  if (buttonState != lastButtonState)
+  {
+    if (buttonState == 0)
+    {
+      pressTime = millis();
+    }
+    else
+    {
+      releaseTime = millis();
+      duration = releaseTime - pressTime;
+    }
+    lastButtonState = buttonState;
+  }
+  while (buttonState == lastButtonState)
+  {
+    if (pressTime <= 100)
+    {
+      client.publish(topic,"Hello STTS World");
+    }
+    else
+    {
+      if (millis() >= Timeout + Timer)
+      {
+        Timer = millis();
+        client.publish(topic,"Hello STTS World");
+      }
+    }
+    buttonState = digitalRead(36);
+  }
+}
